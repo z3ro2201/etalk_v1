ae418ac5d0e40c2c24cd39bc330a532f5cadd135
2ero <ksw1991@protonmail.ch>
2022-04-07 02:25:06 +0900

    수정

--- api/TbCorona19CountStatusJCG.php
+++ api/TbCorona19CountStatusJCG.php
@@ -1,8 +1,7 @@
 <?
 @header('Content-Type:application/json;charset=utf-8');
- error_reporting( E_ALL );
-  ini_set( "display_errors", 1 );
-$url = "http://openapi.seoul.go.kr:8088/504744597876697435334a70766254/json/TbCorona19CountStatus/1/7/";
+$apikey = "";
+$url = "http://openapi.seoul.go.kr:8088/$apikey/json/TbCorona19CountStatus/1/7/";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
